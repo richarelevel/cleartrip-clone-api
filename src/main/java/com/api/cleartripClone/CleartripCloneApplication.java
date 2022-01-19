918974a35cea8a57a8e88c63cabe8b4e3fc59ed2
richasinhaa <richa.sinha0603@gmail.com>
2022-01-19 13:28:27 +0800

    Cleartrip Clone API Template added

---
+++ src/main/java/com/api/cleartripClone/CleartripCloneApplication.java
@@ -0,0 +1,13 @@
+package com.api.cleartripClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleartripCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleartripCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/cleartripClone/CleartripCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cleartripClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleartripCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
